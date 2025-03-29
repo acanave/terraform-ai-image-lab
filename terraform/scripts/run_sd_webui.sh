10daadb4bab47c499b66bb473c293b123fef14d3
acanave <149028341+acanave@users.noreply.github.com>
2025-03-29 15:43:28 -0500

    💾 Add Stable Diffusion WebUI launch script

---
+++ terraform/scripts/run_sd_webui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "🚀 Launching Stable Diffusion WebUI..."
+
+docker run --gpus all \
+  -p 7860:7860 \
+  --rm -it \
+  --name sd-webui \
+  -v $HOME/stable-diffusion/outputs:/outputs \
+  ghcr.io/ashawkey/stable-diffusion-webui
