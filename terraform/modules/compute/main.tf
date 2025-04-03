41f512cf923e74034ce594ac2e4625039d510c82
acanave <149028341+acanave@users.noreply.github.com>
2025-04-02 22:02:01 -0500

    Destroyed infra after t3.large testing, prepping for g4dn.xlarge deployment

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -1,24 +1,38 @@
 resource "aws_instance" "text_to_image_instance" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id
-  vpc_security_group_ids = var.security_group_ids
-  iam_instance_profile   = var.iam_instance_profile
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  subnet_id                   = var.private_subnet_id
+  vpc_security_group_ids      = var.security_group_ids
+  iam_instance_profile        = var.iam_instance_profile
   associate_public_ip_address = false
 
   user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    amazon-linux-extras install docker -y
-    service docker start
-    usermod -a -G docker ec2-user
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              usermod -aG docker ec2-user
 
-    cd /home/ec2-user
-    git clone https://github.com/AbdBarho/stable-diffusion-webui-docker.git
-    cd stable-diffusion-webui-docker
-    docker build -t stable-diffusion-webui:latest .
-    docker run -d --rm -p 7860:7860 stable-diffusion-webui:latest
-  EOF
+              # Install Git
+              yum install git -y
+
+              # Clone the Automatic1111 WebUI repo
+              git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git /home/ec2-user/stable-diffusion-webui
+
+              # Change directory & run the webui (optional, as it can be very GPU-intensive)
+              cd /home/ec2-user/stable-diffusion-webui
+              ./webui.sh --listen
+              EOF
+
+  instance_market_options {
+    market_type = "spot"
+
+    spot_options {
+      spot_instance_type             = "one-time"
+      instance_interruption_behavior = "terminate"
+    }
+  }
 
   tags = {
     Name = "ai-video-lab-text-to-image-instance"
